2868bcdb34343cc90cdb3b7aa711dcb75fcbab2c
ArturT <arturtrzop@gmail.com>
2017-01-15 22:34:30 +0100

    Slow down foo spec only for queue to test both CI nodes fetching from the same queue

--- spec/services/foo_spec.rb
+++ spec/services/foo_spec.rb
@@ -1,7 +1,7 @@
 describe 'Foo' do
   it do
-    if ENV['CI']
-      # slow down on CI so the same queue can start on both CI nodes
+    if ENV['KNAPSACK_PRO_QUEUE_ID']
+      # slow down so the same queue can start on both CI nodes
       sleep 10
     end
     expect(true).to be true
