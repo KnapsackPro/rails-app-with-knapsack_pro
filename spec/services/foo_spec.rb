bdd5d96a432609538b3f03312189c13675d81015
ArturT <arturtrzop@gmail.com>
2017-01-20 21:33:34 +0100

    Foo spec is slow only on CI

--- spec/services/foo_spec.rb
+++ spec/services/foo_spec.rb
@@ -1,6 +1,6 @@
 describe 'Foo' do
   it do
-    if ENV['KNAPSACK_PRO_QUEUE_ID']
+    if ENV['KNAPSACK_PRO_QUEUE_ID'] && ENV['CI']
       # slow down so the same queue can start on both CI nodes
       sleep 10
     end
