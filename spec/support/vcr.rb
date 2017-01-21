64f414d72d7dacfa40e0a706b29b70239aceeae4
ArturT <arturtrzop@gmail.com>
2017-01-21 16:27:57 +0100

    VCR config.ignore_hosts takes arguments instead of arrays

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,14 +4,14 @@ VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock # or :fakeweb
 
-  config.ignore_hosts [
+  config.ignore_hosts(
     'localhost',
     '127.0.0.1',
     '0.0.0.0',
     'api.knapsackpro.com',
     'api-staging.knapsackpro.com',
     'api.knapsackpro.dev',
-  ]
+  )
 end
 
 WebMock.disable_net_connect!(allow: [
