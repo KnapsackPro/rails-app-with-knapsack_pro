9e9206e54c2d83a29c34d43cb56768e8ecb148b3
ArturT <arturtrzop@gmail.com>
2017-04-07 02:48:35 +0200

    require database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
 
+require 'database_cleaner'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
