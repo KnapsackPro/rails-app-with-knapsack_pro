c2cac75fafb3f054d8641790d344536ef1b97742
ArturT <arturtrzop@gmail.com>
2017-05-12 22:58:18 +0200

    Add additional tests to ensure the knapsack_pro detects time correctly

--- spec/time_helpers_spec.rb
+++ spec/time_helpers_spec.rb
@@ -5,6 +5,11 @@ describe 'Time travel with ActiveSupport::Testing::TimeHelpers' do
 
     it do
       expect(Time.current.year).to eq 2004
+      expect(Time.now.year).to eq 2004
+
+      # ensure knapsack_pro adds raw method
+      # to detect real time
+      expect(Time.raw_now.year).to be >= 2017
     end
   end
 
@@ -14,6 +19,7 @@ describe 'Time travel with ActiveSupport::Testing::TimeHelpers' do
     it do
       travel_to(1.day.ago) do
         expect(Time.current.day).to eq yesterday.day
+        expect(Time.raw_now.year).to be >= 2017
       end
     end
   end
