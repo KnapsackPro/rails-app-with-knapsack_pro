afac46393c5c7499983b66984604e64546514e67
ArturT <arturtrzop@gmail.com>
2017-08-18 17:22:21 +0200

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 20150501111835) do
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
