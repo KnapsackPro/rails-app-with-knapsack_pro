e709e0d4a370ada67db6a40b38dd32b9e1bebfbd
ArturT <arturtrzop@gmail.com>
2017-09-17 22:07:23 +0200

    Print info about startup and shutdown of particular test unit file.

--- test-unit/models/foo_test.rb
+++ test-unit/models/foo_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class FooTest < Test::Unit::TestCase
+  class << self
+    def startup
+      puts 'STARTUP --------'
+    end
+
+    def shutdown
+      puts 'SHUTDOWN --------'
+    end
+  end
+
   test "the truth" do
     assert true
   end
