eaf99f9e2de50dc1fc30e0fc8238fccc8f0c7ee7
ArturT <arturtrzop@gmail.com>
2017-10-31 01:32:56 +0100

    flaky test for cucumber config

--- features/step_definitions/welcome_steps.rb
+++ features/step_definitions/welcome_steps.rb
@@ -7,5 +7,9 @@ When /^I click on calculator link$/ do
 end
 
 When /^I should be on calculator page$/ do
+  # flaky test for cucumber config:
+  # --retry 5 --no-strict-flaky]
+  #expect([1,2].sample).to eq 1
+
   expect(current_path).to eq calculator_index_path
 end
