5d484bd0a7034f3c2c74cd07c3b912e0476f7ed0
ArturT <arturtrzop@gmail.com>
2017-11-10 12:37:05 +0100

    Fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20150501111835) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
