35a1292b8495161815b3fb77c29df6674b6fb010
ArturT <arturtrzop@gmail.com>
2018-05-03 14:05:11 +0200

    Show only 10 dashes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,21 +7,21 @@ require 'knapsack_pro'
 
 # CUSTOM_CONFIG_GOES_HERE
 KnapsackPro::Hooks::Queue.before_queue do |queue_id|
-  print '-'*20
+  print '-'*10
   print 'Before Queue Hook - run before test suite'
-  print '-'*20
+  print '-'*10
 end
 
 KnapsackPro::Hooks::Queue.after_subset_queue do |queue_id, subset_queue_id|
-  print '-'*20
+  print '-'*10
   print 'After Subset Queue Hook - run after subset of test suite'
-  print '-'*20
+  print '-'*10
 end
 
 KnapsackPro::Hooks::Queue.after_queue do |queue_id|
-  print '-'*20
+  print '-'*10
   print 'After Queue Hook - run after test suite'
-  print '-'*20
+  print '-'*10
 end
 
 knapsack_pro_adapter = KnapsackPro::Adapters::MinitestAdapter.bind
