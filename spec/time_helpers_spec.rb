bbb1e5a5f198b63c52b3e9745aba4beba352acdb
ArturT <arturtrzop@gmail.com>
2018-06-12 09:06:54 +0200

    Slow down tests with time travel to check if knapsack_pro measures the time correctly

--- spec/time_helpers_spec.rb
+++ spec/time_helpers_spec.rb
@@ -1,9 +1,15 @@
+# We on purpose slown down each test by 1s.
+# We expect to see the file to took ~3s in knapsack pro dashboard.
+# This way we can validate if the ActiveSupport::Testing::TimeHelpers
+# have impact on the way how knapsack_pro measures the timing for tests.
 describe 'Time travel with ActiveSupport::Testing::TimeHelpers' do
   context 'travel_back' do
     before { travel_to Time.new(2004, 11, 24, 01, 04, 44) }
     after { travel_back }
 
     it do
+      sleep 1
+
       expect(Time.current.year).to eq 2004
       expect(Time.now.year).to eq 2004
 
@@ -18,6 +24,8 @@ describe 'Time travel with ActiveSupport::Testing::TimeHelpers' do
 
     it do
       travel_to(1.day.ago) do
+        sleep 1
+
         expect(Time.current.day).to eq yesterday.day
         expect(Time.raw_now.year).to be >= 2017
       end
@@ -29,6 +37,8 @@ describe 'Time travel with ActiveSupport::Testing::TimeHelpers' do
 
     it do
       travel_to(time_2014) do
+        sleep 1
+
         expect(Time.current.year).to eq 2004
         expect(Time.raw_now.year).to be >= 2017
       end
