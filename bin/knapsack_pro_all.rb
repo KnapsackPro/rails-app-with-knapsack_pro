5498339a52ca75edd19df993d8b639d7a8171aed
Artur Trzop <arturtrzop@gmail.com>
2018-11-02 15:50:32 +0100

    Add test file exclude pattern example for Regular and Queue Mode

--- bin/knapsack_pro_all.rb
+++ bin/knapsack_pro_all.rb
@@ -18,6 +18,9 @@ COMMANDS = %{
 ./bin/knapsack_pro_rspec_test_dir 0 2
 ./bin/knapsack_pro_rspec_test_dir 1 2
 
+./bin/knapsack_pro_rspec_test_file_exclude_pattern 0 2
+./bin/knapsack_pro_rspec_test_file_exclude_pattern 1 2
+
 ./bin/knapsack_pro_queue_rspec 0 2
 ./bin/knapsack_pro_queue_rspec 1 2
 
@@ -38,6 +41,9 @@ COMMANDS = %{
 
 ./bin/knapsack_pro_queue_rspec_test_dir 0 2
 ./bin/knapsack_pro_queue_rspec_test_dir 1 2
+
+./bin/knapsack_pro_queue_rspec_test_file_exclude_pattern 0 2
+./bin/knapsack_pro_queue_rspec_test_file_exclude_pattern 1 2
 
 ./bin/knapsack_pro_queue_rspec_initialized_once 0 2
 ./bin/knapsack_pro_queue_rspec_initialized_once 1 2
