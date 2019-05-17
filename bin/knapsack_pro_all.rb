c3ba191efd94c0078d225e97b6589ab9af6750b8
Artur Trzop <arturtrzop@gmail.com>
2019-05-17 22:30:14 +0200

    Add bin to run knapsack pro with KNAPSACK_PRO_TEST_FILE_LIST

--- bin/knapsack_pro_all.rb
+++ bin/knapsack_pro_all.rb
@@ -21,6 +21,9 @@ COMMANDS = %{
 ./bin/knapsack_pro_rspec_test_file_exclude_pattern 0 2
 ./bin/knapsack_pro_rspec_test_file_exclude_pattern 1 2
 
+./bin/knapsack_pro_queue_rspec_test_file_list 0 2
+./bin/knapsack_pro_queue_rspec_test_file_list 1 2
+
 ./bin/knapsack_pro_queue_rspec 0 2
 ./bin/knapsack_pro_queue_rspec 1 2
 
