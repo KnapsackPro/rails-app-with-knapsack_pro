ad892a47dab3d18f116725213c24066d98e7bd46
Artur Trzop <arturtrzop@gmail.com>
2020-03-06 12:23:31 +0100

    Update welcome_steps.rb
    add example failing expects

--- features/step_definitions/welcome_steps.rb
+++ features/step_definitions/welcome_steps.rb
@@ -11,5 +11,8 @@ When /^I should be on calculator page$/ do
   # --retry 5 --no-strict-flaky]
   #expect([1,2].sample).to eq 1
 
+  #expect(2).to eq 1
+  #expect(find('#task1 .desc')).to eq 'fake'
+
   expect(current_path).to eq calculator_index_path
 end
