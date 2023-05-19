8a1f8df3823ed8170ccd4ff2f8e6ad58f396bc37
3v0k4 <riccardo.odone@gmail.com>
2023-05-19 15:54:49 +0200

    test: update scripts in bin to use build id

--- bin/knapsack_pro_all.rb
+++ bin/knapsack_pro_all.rb
@@ -1,194 +1,97 @@
-COMMANDS = %{
-./bin/knapsack_pro_a_few_commands 0 2
-./bin/knapsack_pro_a_few_commands 1 2
+require 'securerandom'
 
-# RSpec
-./bin/knapsack_pro_rspec 0 2
-./bin/knapsack_pro_rspec 1 2
+COMMANDS = {
+  './bin/knapsack_pro_a_few_commands' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
 
-./bin/knapsack_pro_rspec_user_seat 0 2
-./bin/knapsack_pro_rspec_user_seat 1 2
+  # RSpec
+  './bin/knapsack_pro_rspec' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_rspec_user_seat' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_rspec_cache_read_attempt' => ['0 2', '1 2'],
+  './bin/knapsack_pro_rspec_junit' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_rspec_only_partial_nodes' => ['0 3 BUILD_ID', '1 3 BUILD_ID'],
+  './bin/knapsack_pro_rspec_encrypted' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_rspec_disabled' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_rspec_fallback_mode' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_rspec_test_dir' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_rspec_test_file_exclude_pattern' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_rspec_split_by_test_examples' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_test_file_list' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_test_file_list_source_file' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_user_seat' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_record_first_run' => ['0 2', '1 2'],
+  './bin/knapsack_pro_queue_rspec_split_by_test_examples' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_split_by_test_examples_test_example_detector_prefix' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_tags' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_default_formatter' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_profile_formatter' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_junit' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_log_dir' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_json' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_test_dir' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_test_file_exclude_pattern' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_frequently_changing_test_files' => [''],
+  './bin/knapsack_pro_queue_rspec_initialized_once' => ['0 2', '1 2'],
+  './bin/knapsack_pro_queue_rspec_only_failures' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_order_defined' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_order_rand' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_order_rand_with_custom_seed' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_order_random' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_order_random_defined_in_config' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_fixed_queue_rspec_queue_consumed_at_least_once_with_ci_build_id' => [''],
+  './bin/knapsack_pro_fixed_queue_split_rspec' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_fixed_queue_split_rspec_custom_branch_commit' => ['0 2 branch-name BUILD_ID', '1 2 branch-name BUILD_ID'],
+  './bin/knapsack_pro_queue_rspec_encrypted' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_fixed_queue_split_rspec_encrypted' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/parallel_tests_knapsack_pro_queue_rspec' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/parallel_tests_knapsack_pro_single_machine_run BUILD_ID' => [''],
+  './bin/bin_knapsack_pro_rspec' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/bin_knapsack_pro_queue_rspec' => ['0 2 same-build-id', '1 2 same-build-id'],
 
-./bin/knapsack_pro_rspec_cache_read_attempt 0 2
-./bin/knapsack_pro_rspec_cache_read_attempt 1 2
+  # inside of this bash scripts we run 2 parallel nodes
+  './bin/knapsack_pro_split_by_test_cases_rspec' => [''],
+  './bin/knapsack_pro_split_by_test_cases_queue_rspec' => [''],
 
-./bin/knapsack_pro_rspec_junit 0 2
-./bin/knapsack_pro_rspec_junit 1 2
+  # Cucumber
+  './bin/knapsack_pro_cucumber' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_cucumber_junit' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_cucumber' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_cucumber_junit' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_cucumber_prefix' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
 
-./bin/knapsack_pro_rspec_only_partial_nodes 0 3
-./bin/knapsack_pro_rspec_only_partial_nodes 1 3
+  # Minitest
+  './bin/knapsack_pro_minitest' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_queue_minitest' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+  './bin/knapsack_pro_fixed_queue_split_minitest_custom_branch_commit' => ['0 2 branch-name BUILD_ID', '1 2 branch-name BUILD_ID'],
+  './bin/bin_knapsack_pro_queue_minitest' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
 
-./bin/knapsack_pro_rspec_encrypted 0 2
-./bin/knapsack_pro_rspec_encrypted 1 2
+  # Test::Unit
+  './bin/knapsack_pro_test_unit' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
 
-./bin/knapsack_pro_rspec_disabled 0 2
-./bin/knapsack_pro_rspec_disabled 1 2
+  # Spinach
+  './bin/knapsack_pro_spinach' => ['0 2 BUILD_ID', '1 2 BUILD_ID'],
+}
 
-./bin/knapsack_pro_rspec_fallback_mode 0 2
-./bin/knapsack_pro_rspec_fallback_mode 1 2
-
-./bin/knapsack_pro_rspec_test_dir 0 2
-./bin/knapsack_pro_rspec_test_dir 1 2
-
-./bin/knapsack_pro_rspec_test_file_exclude_pattern 0 2
-./bin/knapsack_pro_rspec_test_file_exclude_pattern 1 2
-
-./bin/knapsack_pro_rspec_split_by_test_examples 0 2
-./bin/knapsack_pro_rspec_split_by_test_examples 1 2
-
-./bin/knapsack_pro_queue_rspec_test_file_list 0 2
-./bin/knapsack_pro_queue_rspec_test_file_list 1 2
-
-./bin/knapsack_pro_queue_rspec_test_file_list_source_file 0 2
-./bin/knapsack_pro_queue_rspec_test_file_list_source_file 1 2
-
-./bin/knapsack_pro_queue_rspec 0 2
-./bin/knapsack_pro_queue_rspec 1 2
-
-./bin/knapsack_pro_queue_rspec_user_seat 0 2
-./bin/knapsack_pro_queue_rspec_user_seat 1 2
-
-./bin/knapsack_pro_queue_rspec_record_first_run 0 2
-./bin/knapsack_pro_queue_rspec_record_first_run 1 2
-
-./bin/knapsack_pro_queue_rspec_split_by_test_examples 0 2
-./bin/knapsack_pro_queue_rspec_split_by_test_examples 1 2
-
-./bin/knapsack_pro_queue_rspec_split_by_test_examples_test_example_detector_prefix 0 2
-./bin/knapsack_pro_queue_rspec_split_by_test_examples_test_example_detector_prefix 1 2
-
-./bin/knapsack_pro_queue_rspec_tags 0 2
-./bin/knapsack_pro_queue_rspec_tags 1 2
-
-./bin/knapsack_pro_queue_rspec_default_formatter 0 2
-./bin/knapsack_pro_queue_rspec_default_formatter 1 2
-
-./bin/knapsack_pro_queue_rspec_profile_formatter 0 2
-./bin/knapsack_pro_queue_rspec_profile_formatter 1 2
-
-./bin/knapsack_pro_queue_rspec_junit 0 2
-./bin/knapsack_pro_queue_rspec_junit 1 2
-
-./bin/knapsack_pro_queue_rspec_log_dir 0 2
-./bin/knapsack_pro_queue_rspec_log_dir 1 2
-
-./bin/knapsack_pro_queue_rspec_json 0 2
-./bin/knapsack_pro_queue_rspec_json 1 2
-
-./bin/knapsack_pro_queue_rspec_test_dir 0 2
-./bin/knapsack_pro_queue_rspec_test_dir 1 2
-
-./bin/knapsack_pro_queue_rspec_test_file_exclude_pattern 0 2
-./bin/knapsack_pro_queue_rspec_test_file_exclude_pattern 1 2
-
-./bin/knapsack_pro_queue_rspec_frequently_changing_test_files
-
-./bin/knapsack_pro_queue_rspec_initialized_once 0 2
-./bin/knapsack_pro_queue_rspec_initialized_once 1 2
-
-./bin/knapsack_pro_queue_rspec_only_failures 0 2
-./bin/knapsack_pro_queue_rspec_only_failures 1 2
-
-./bin/knapsack_pro_queue_rspec_order_defined 0 2
-./bin/knapsack_pro_queue_rspec_order_defined 1 2
-
-./bin/knapsack_pro_queue_rspec_order_rand 0 2
-./bin/knapsack_pro_queue_rspec_order_rand 1 2
-
-./bin/knapsack_pro_queue_rspec_order_rand_with_custom_seed 0 2
-./bin/knapsack_pro_queue_rspec_order_rand_with_custom_seed 1 2
-
-./bin/knapsack_pro_queue_rspec_order_random 0 2
-./bin/knapsack_pro_queue_rspec_order_random 1 2
-
-./bin/knapsack_pro_queue_rspec_order_random_defined_in_config 0 2
-./bin/knapsack_pro_queue_rspec_order_random_defined_in_config 1 2
-
-./bin/knapsack_pro_fixed_queue_rspec_queue_consumed_at_least_once_with_ci_build_id
-
-./bin/knapsack_pro_fixed_queue_rspec_queue_consumed_at_least_once_without_ci_build_id
-
-./bin/knapsack_pro_fixed_queue_split_rspec 0 2
-./bin/knapsack_pro_fixed_queue_split_rspec 1 2
-
-./bin/knapsack_pro_fixed_queue_split_rspec_custom_branch_commit 0 2
-./bin/knapsack_pro_fixed_queue_split_rspec_custom_branch_commit 1 2
-
-./bin/knapsack_pro_queue_rspec_encrypted 0 2
-./bin/knapsack_pro_queue_rspec_encrypted 1 2
-
-./bin/knapsack_pro_fixed_queue_split_rspec_encrypted 0 2
-./bin/knapsack_pro_fixed_queue_split_rspec_encrypted 1 2
-
-./bin/parallel_tests_knapsack_pro_queue_rspec 0 2
-./bin/parallel_tests_knapsack_pro_queue_rspec 1 2
-
-./bin/parallel_tests_knapsack_pro_single_machine_run
-
-./bin/bin_knapsack_pro_rspec 0 2
-./bin/bin_knapsack_pro_rspec 1 2
-
-./bin/bin_knapsack_pro_queue_rspec 0 2
-./bin/bin_knapsack_pro_queue_rspec 1 2
-
-# inside of this bash scripts we run 2 parallel nodes
-./bin/knapsack_pro_split_by_test_cases_rspec
-./bin/knapsack_pro_split_by_test_cases_queue_rspec
-
-# Cucumber
-./bin/knapsack_pro_cucumber 0 2
-./bin/knapsack_pro_cucumber 1 2
-
-./bin/knapsack_pro_cucumber_junit 0 2
-./bin/knapsack_pro_cucumber_junit 1 2
-
-./bin/knapsack_pro_queue_cucumber 0 2
-./bin/knapsack_pro_queue_cucumber 1 2
-
-./bin/knapsack_pro_queue_cucumber_junit 0 2
-./bin/knapsack_pro_queue_cucumber_junit 1 2
-
-./bin/knapsack_pro_queue_cucumber_prefix 0 2
-./bin/knapsack_pro_queue_cucumber_prefix 1 2
-
-# Minitest
-./bin/knapsack_pro_minitest 0 2
-./bin/knapsack_pro_minitest 1 2
-
-./bin/knapsack_pro_queue_minitest 0 2
-./bin/knapsack_pro_queue_minitest 1 2
-
-./bin/knapsack_pro_fixed_queue_split_minitest_custom_branch_commit 0 2
-./bin/knapsack_pro_fixed_queue_split_minitest_custom_branch_commit 1 2
-
-./bin/bin_knapsack_pro_queue_minitest 0 2
-./bin/bin_knapsack_pro_queue_minitest 1 2
-
-# Test::Unit
-./bin/knapsack_pro_test_unit 0 2
-./bin/knapsack_pro_test_unit 1 2
-
-# Spinach
-./bin/knapsack_pro_spinach 0 2
-./bin/knapsack_pro_spinach 1 2
-}.split("\n").select { |i| i =~ /\A\.\/bin\// }
-
-
-final_status = 0
 failed_commands = []
 
-COMMANDS.each do |command|
-  system(command)
-  unless $?.exitstatus == 0
-    final_status = $?.exitstatus
-    failed_commands << command
+COMMANDS.each do |command, args|
+  uuid = SecureRandom.uuid
+
+  args
+    .map { _1.sub('BUILD_ID', uuid) }
+    .each do |arg|
+      cmd = [command, arg].join(' ')
+      puts "EXECUTING: #{cmd}"
+      system(cmd)
+
+      (failed_commands << command) if $?.exitstatus != 0
   end
 end
 
 puts
-puts '='*20
+puts '=' * 20
 puts
-if final_status == 0
+if failed_commands.empty?
   puts 'All tests pass with success!'
 else
   puts 'Something failed! You can retry those commands:'
