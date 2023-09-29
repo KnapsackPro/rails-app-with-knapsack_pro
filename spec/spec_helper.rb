94f2a38afd3129b8c740e7c1af7a2d302256ba7b
Artur Trzop <arturtrzop@gmail.com>
2023-09-29 14:13:01 +0200

    add examples how to run queue mode hooks only in queue mode and still be able to run RSpec hooks when not using queue mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,6 +30,49 @@ require 'knapsack_pro'
 require 'simplecov'
 SimpleCov.start
 
+
+before_suite_block = proc {
+  puts '+'*100
+  puts 'RSpec before suite hook called only once'
+}
+
+unless KnapsackPro::Config::Env.queue_recording_enabled?
+  RSpec.configure do |config|
+    config.before(:suite) do
+      before_suite_block.call
+      puts 'Run this only when not using Knapsack Pro Queue Mode'
+    end
+  end
+end
+
+KnapsackPro::Hooks::Queue.before_queue do |queue_id|
+  before_suite_block.call
+  puts 'Run this only when using Knapsack Pro Queue Mode'
+  puts 'This code is executed within the context of RSpec before(:suite) hook'
+end
+
+
+after_suite_block = proc {
+  puts '+'*100
+  puts 'after suite hook called only once'
+}
+
+unless KnapsackPro::Config::Env.queue_recording_enabled?
+  RSpec.configure do |config|
+    config.after(:suite) do
+      after_suite_block.call
+      puts 'Run this only when not using Knapsack Pro Queue Mode'
+    end
+  end
+end
+
+KnapsackPro::Hooks::Queue.after_queue do |queue_id|
+  after_suite_block.call
+  puts 'Run this only when using Knapsack Pro Queue Mode'
+  puts "This code is executed outside of the RSpec after(:suite) hook context because it's impossible to determine which after(:suite) is the last one to execute until it's executed."
+end
+
+
 # CUSTOM_CONFIG_GOES_HERE
 KnapsackPro::Hooks::Queue.before_queue do |queue_id|
   print '-'*10
