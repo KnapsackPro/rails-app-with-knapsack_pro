74bb5de3374a39f3b734df8e08f176cbcd2664a8
Artur Trzop <arturtrzop@gmail.com>
2024-01-15 14:25:41 +0100

    remove unneeded before_suite_block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,27 +31,6 @@ require 'simplecov'
 SimpleCov.start
 
 
-before_suite_block = proc {
-  puts '+'*100
-  puts 'RSpec before suite hook called only once'
-}
-
-unless KnapsackPro::Config::Env.queue_recording_enabled?
-  RSpec.configure do |config|
-    config.before(:suite) do
-      before_suite_block.call
-      puts 'Run this only when not using Knapsack Pro Queue Mode'
-    end
-  end
-end
-
-KnapsackPro::Hooks::Queue.before_queue do |queue_id|
-  before_suite_block.call
-  puts 'Run this only when using Knapsack Pro Queue Mode'
-  puts 'This code is executed within the context of RSpec before(:suite) hook'
-end
-
-
 # CUSTOM_CONFIG_GOES_HERE
 KnapsackPro::Hooks::Queue.before_queue do |queue_id|
   print '-'*10
