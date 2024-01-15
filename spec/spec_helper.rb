a1db61f05fab5e4d6ab8e6b017764296a8b6a220
Artur Trzop <arturtrzop@gmail.com>
2024-01-15 12:49:12 +0100

    remove not needed after_suite_block.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,27 +52,6 @@ KnapsackPro::Hooks::Queue.before_queue do |queue_id|
 end
 
 
-after_suite_block = proc {
-  puts '+'*100
-  puts 'after suite hook called only once'
-}
-
-unless KnapsackPro::Config::Env.queue_recording_enabled?
-  RSpec.configure do |config|
-    config.after(:suite) do
-      after_suite_block.call
-      puts 'Run this only when not using Knapsack Pro Queue Mode'
-    end
-  end
-end
-
-KnapsackPro::Hooks::Queue.after_queue do |queue_id|
-  after_suite_block.call
-  puts 'Run this only when using Knapsack Pro Queue Mode'
-  puts "This code is executed outside of the RSpec after(:suite) hook context because it's impossible to determine which after(:suite) is the last one to execute until it's executed."
-end
-
-
 # CUSTOM_CONFIG_GOES_HERE
 KnapsackPro::Hooks::Queue.before_queue do |queue_id|
   print '-'*10
