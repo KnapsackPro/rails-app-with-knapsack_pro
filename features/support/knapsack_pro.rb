4c3eea2d9245d73f3b5bc670631bf472ec88b348
Artur Trzop <arturtrzop@gmail.com>
2024-05-08 21:12:01 +0200

    Log queue batches

--- features/support/knapsack_pro.rb
+++ features/support/knapsack_pro.rb
@@ -5,18 +5,30 @@ KnapsackPro::Hooks::Queue.before_queue do |queue_id|
   print '-'*10
   print 'Before Queue Hook - run before test suite'
   print '-'*10
+
+  puts
+  puts 'Queue Batches:'
+  puts KnapsackPro::Store::Server.client.queue_batches.inspect
 end
 
 KnapsackPro::Hooks::Queue.after_subset_queue do |queue_id, subset_queue_id|
   print '-'*10
   print 'After Subset Queue Hook - run after subset of test suite'
   print '-'*10
+
+  puts
+  puts 'Queue Batches:'
+  puts KnapsackPro::Store::Server.client.queue_batches.inspect
 end
 
 KnapsackPro::Hooks::Queue.after_queue do |queue_id|
   print '-'*10
   print 'After Queue Hook - run after test suite'
   print '-'*10
+
+  puts
+  puts 'Queue Batches:'
+  puts KnapsackPro::Store::Server.client.queue_batches.inspect
 end
 
 KnapsackPro::Adapters::CucumberAdapter.bind
