933a72e1e8bd7524213b73b3d5264da1c6e8d5df
Artur Trzop <arturtrzop@gmail.com>
2024-05-08 21:12:01 +0200

    Add all queue hooks examples for RSpec, Cucumber, Minitest

--- features/support/knapsack_pro.rb
+++ features/support/knapsack_pro.rb
@@ -3,19 +3,25 @@ require 'knapsack_pro'
 # CUSTOM_CONFIG_GOES_HERE
 KnapsackPro::Hooks::Queue.before_queue do |queue_id|
   print '-'*10
-  print 'Before Queue Hook - run before test suite'
+  print 'Before Queue Hook - run before the test suite'
+  print '-'*10
+end
+
+KnapsackPro::Hooks::Queue.before_subset_queue do |queue_id, subset_queue_id|
+  print '-'*10
+  print 'Before Subset Queue Hook - run before the next subset of tests'
   print '-'*10
 end
 
 KnapsackPro::Hooks::Queue.after_subset_queue do |queue_id, subset_queue_id|
   print '-'*10
-  print 'After Subset Queue Hook - run after subset of test suite'
+  print 'After Subset Queue Hook - run after the previous subset of tests'
   print '-'*10
 end
 
 KnapsackPro::Hooks::Queue.after_queue do |queue_id|
   print '-'*10
-  print 'After Queue Hook - run after test suite'
+  print 'After Queue Hook - run after the test suite'
   print '-'*10
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,9 +34,8 @@ SimpleCov.start
 # CUSTOM_CONFIG_GOES_HERE
 KnapsackPro::Hooks::Queue.before_queue do |queue_id|
   print '-'*10
-  print 'Before Queue Hook - run before test suite'
+  print 'Before Queue Hook - run before the test suite'
   print '-'*10
-  puts
 
   SimpleCov.command_name("rspec_ci_node_#{KnapsackPro::Config::Env.ci_node_index}")
 end
@@ -46,9 +45,8 @@ end
 
 KnapsackPro::Hooks::Queue.before_subset_queue do |queue_id, subset_queue_id|
   print '-'*10
-  print 'Before Subset Queue Hook - run before the subset of the test suite'
+  print 'Before Subset Queue Hook - run before the next subset of tests'
   print '-'*10
-  puts
 end
 KnapsackPro::Hooks::Queue.before_subset_queue do |queue_id, subset_queue_id|
   puts '2nd KnapsackPro::Hooks::Queue.before_subset_queue'
@@ -56,9 +54,8 @@ end
 
 KnapsackPro::Hooks::Queue.after_subset_queue do |queue_id, subset_queue_id|
   print '-'*10
-  print 'After Subset Queue Hook - run after the subset of the test suite'
+  print 'After Subset Queue Hook - run after the previous subset of tests'
   print '-'*10
-  puts
 end
 KnapsackPro::Hooks::Queue.after_subset_queue do |queue_id, subset_queue_id|
   puts '2nd KnapsackPro::Hooks::Queue.after_subset_queue'
@@ -66,9 +63,8 @@ end
 
 KnapsackPro::Hooks::Queue.after_queue do |queue_id|
   print '-'*10
-  print 'After Queue Hook - run after test suite'
+  print 'After Queue Hook - run after the test suite'
   print '-'*10
-  puts
 end
 KnapsackPro::Hooks::Queue.after_queue do |queue_id|
   puts '2nd KnapsackPro::Hooks::Queue.after_queue'
