29ea2e8a261dcc3b30d73e449d5ac8555fcba553
Artur Trzop <arturtrzop@gmail.com>
2024-05-14 23:52:19 +0200

    log queue batches in RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,7 +36,10 @@ KnapsackPro::Hooks::Queue.before_queue do |queue_id|
   print '-'*10
   print 'Before Queue Hook - run before the test suite'
   print '-'*10
+
   puts
+  print 'Batches in before_queue: '
+  puts KnapsackPro::Store::Client.batches.inspect
 
   SimpleCov.command_name("rspec_ci_node_#{KnapsackPro::Config::Env.ci_node_index}")
 end
@@ -48,7 +51,10 @@ KnapsackPro::Hooks::Queue.before_subset_queue do |queue_id, subset_queue_id|
   print '-'*10
   print 'Before Subset Queue Hook - run before the next subset of tests'
   print '-'*10
+
   puts
+  print 'Batches in before_subset_queue: '
+  puts KnapsackPro::Store::Client.batches.inspect
 end
 KnapsackPro::Hooks::Queue.before_subset_queue do |queue_id, subset_queue_id|
   puts '2nd KnapsackPro::Hooks::Queue.before_subset_queue'
@@ -58,7 +64,10 @@ KnapsackPro::Hooks::Queue.after_subset_queue do |queue_id, subset_queue_id|
   print '-'*10
   print 'After Subset Queue Hook - run after the previous subset of tests'
   print '-'*10
+
   puts
+  print 'Batches in after_subset_queue: '
+  puts KnapsackPro::Store::Client.batches.inspect
 end
 KnapsackPro::Hooks::Queue.after_subset_queue do |queue_id, subset_queue_id|
   puts '2nd KnapsackPro::Hooks::Queue.after_subset_queue'
@@ -68,7 +77,10 @@ KnapsackPro::Hooks::Queue.after_queue do |queue_id|
   print '-'*10
   print 'After Queue Hook - run after the test suite'
   print '-'*10
+
   puts
+  print 'Batches in after_queue: '
+  puts KnapsackPro::Store::Client.batches.inspect
 end
 KnapsackPro::Hooks::Queue.after_queue do |queue_id|
   puts '2nd KnapsackPro::Hooks::Queue.after_queue'
