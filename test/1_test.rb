9139446ca7762976c17f5f083a1592a19c78e6d5
Artur Trzop <arturtrzop@gmail.com>
2024-06-06 16:14:43 +0200

    debug: reproduce error
    rake aborted!
    test_#index is already defined in WelcomeControllerTest

---
+++ test/1_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    # blah
+  end
+
+  test '#index' do
+    # test goes here
+  end
+
+  # if I uncomment the 2nd test case within the same class (WelcomeControllerTest) then it would fail
+  #test '#index' do
+    ## test goes here
+  #end
+end
---
+++ test/2_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    # blah
+  end
+
+  test '#index' do
+    # test goes here
+  end
+end
