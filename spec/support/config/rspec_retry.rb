4bca63cd18d5f1ddf8e24fb3e1828339ec39b40d
Artur Trzop <arturtrzop@gmail.com>
2024-06-12 20:01:42 +0200

    Update rspec_retry.rb

--- spec/support/config/rspec_retry.rb
+++ spec/support/config/rspec_retry.rb
@@ -5,7 +5,7 @@ RSpec.configure do |config|
   config.verbose_retry = true
   # show exception that triggers a retry if verbose_retry is set to true
   config.display_try_failure_messages = true
- 
+
   # --fail-fast option makes RSpec fail after X failed tests. This can lead to canceled tests in Queue Mode.
   # config.fail_fast = 1
 
