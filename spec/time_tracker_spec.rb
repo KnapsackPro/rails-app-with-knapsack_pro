01a25ad12c5a3eabe16a8b6b758ffb0928d57434
Artur Trzop <arturtrzop@gmail.com>
2024-06-26 21:03:47 +0200

    Create time_tracker_spec.rb
    add spec file that explicitly requires spec_helper

---
+++ spec/time_tracker_spec.rb
@@ -0,0 +1,9 @@
+# Needed to reproduce the issue:
+# https://github.com/KnapsackPro/knapsack_pro-ruby/pull/265
+require 'spec_helper'
+
+describe 'Test Time Tracker' do
+  it do
+    expect(true).to be true
+  end
+end
