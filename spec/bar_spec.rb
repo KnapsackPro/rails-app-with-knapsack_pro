bbb34a606c80f3ac0e60e2e3e0113fdf0c9c7a24
Artur Trzop <arturtrzop@gmail.com>
2025-02-25 00:56:04 +0100

    add tags to foo and bar

--- spec/bar_spec.rb
+++ spec/bar_spec.rb
@@ -1,5 +1,15 @@
 describe 'Bar', :tag_a do
-  it do
+  it 'Bar 1' do
     expect(Article.new).to be_kind_of Article
+  end
+
+  it 'Bar 2 - tag_x', :tag_x do
+  end
+
+  it 'Bar 3 - tag_x', :tag_x do
+  end
+
+  it 'Bar 4 - tag_x AND tag_y', :tag_x, :tag_y do
+
   end
 end
