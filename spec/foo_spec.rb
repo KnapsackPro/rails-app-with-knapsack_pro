d44d3600bcdd9bf0e62ae7e558d5083f5d91fc93
Artur Trzop <arturtrzop@gmail.com>
2025-02-25 17:10:41 +0100

    Add tests to test SBTE + RSpec tags on CI (#72)

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -1,5 +1,11 @@
 describe 'Foo' do
-  it do
+  it 'Foo 1 - tag_x', :tag_x do
+  end
+
+  it 'Foo 2' do
+  end
+
+  it 'Foo 3' do
     if ENV['KNAPSACK_PRO_QUEUE_ID'] && ENV['CI']
       # slow down so the same queue can start on both CI nodes
       sleep 10
--- turnip/acceptance/foo.feature
+++ turnip/acceptance/foo.feature
@@ -2,6 +2,10 @@ Feature: Foo
   Background:
     Given I have a two
 
-  Scenario: Add one
+  Scenario: Add one (1st)
+    When I add one
+    Then I expect a three
+
+  Scenario: Add one (2nd)
     When I add one
     Then I expect a three
