e6313b0b4dcd4e8796f5940b1d75d994c582560f
Artur Trzop <arturtrzop@gmail.com>
2025-04-03 23:27:55 +0200

    test: rake tasks

--- spec/rake_tasks/dummy_rake_spec.rb
+++ spec/rake_tasks/dummy_rake_spec.rb
@@ -1,34 +1,26 @@
-require 'rake'
-
 describe 'Dummy rake' do
   describe "dummy:do_something_once" do
     let(:task_name) { "dummy:do_something_once" }
     let(:task) { Rake::Task[task_name] }
 
-    before do
-      # clear the rake task from the memory to ensure it's not loaded multiple times
-      Rake::Task[task_name].clear if Rake::Task.task_defined?(task_name)
+    context 'when Rake.application.rake_require is used to load the rake task' do
+      before do
+        Rake.application.rake_require("tasks/dummy")
+        Rake::Task.define_task(:environment)
+      end
 
-      # loaad the rake task only once
-      Rake.load_rakefile("tasks/dummy.rake")
-      Rake::Task.define_task(:environment)
-    end
+      after do
+        Rake::Task[task_name].reenable
 
-    after do
-      Rake::Task[task_name].reenable
+        DummyOutput.count = 0
+      end
 
-      # reset the state that was changed by the rake task execution
-      DummyOutput.count = 0
-    end
-
-    it "calls the rake task once (increases counter by one)" do
-      expect { task.invoke }.to_not raise_error
-      expect(DummyOutput.count).to eq(1)
-    end
-
-    it "calls the rake task once again (increases counter by one)" do
-      expect { task.invoke }.to_not raise_error
-      expect(DummyOutput.count).to eq(1)
+      2.times do
+        it "calls the rake task (increases counter by one)" do
+          expect { task.invoke }.to_not raise_error
+          expect(DummyOutput.count).to eq(1)
+        end
+      end
     end
   end
 end
